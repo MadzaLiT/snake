b5330638f3a9d61fa27d23192f9b3bf7613a5e01
MaD <madzalit@gmail.com>
2023-02-04 15:37:55 +0300

    Hello World

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Security;
+using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
